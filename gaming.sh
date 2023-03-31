80b4fb09dfb5008d570324a90c8ead88c578e904
youdontknowdemo <36462734+youdontknowdemo@users.noreply.github.com>
2023-03-31 02:02:14 -0600

    Update gaming.sh

--- gaming.sh
+++ gaming.sh
@@ -3,39 +3,39 @@
 ################################################
 ##### MangoHud
 ################################################
-
+sudo -u ${NEW_USER} paru -S --noconfirm goverlay-bin
 # References:
-# https://github.com/flathub/com.valvesoftware.Steam.Utility.MangoHud
-
-# Install MangoHud
-flatpak install -y flathub org.freedesktop.Platform.VulkanLayer.MangoHud/x86_64/22.08
-
-# Configure MangoHud
-mkdir -p /home/${NEW_USER}/.config/MangoHud
-
-tee /home/${NEW_USER}/.config/MangoHud/MangoHud.conf << EOF
-engine_version
-vulkan_driver
-EOF
-
-# Allow Flatpaks to access MangoHud configs
-flatpak override --filesystem=xdg-config/MangoHud:ro
+# None yet
 
 ################################################
-##### Platforms
+##### Steam
 ################################################
 
-# Steam
-flatpak install -y flathub com.valvesoftware.Steam
-flatpak install -y flathub com.valvesoftware.Steam.Utility.gamescope
-flatpak install -y flathub com.valvesoftware.Steam.CompatibilityTool.Proton-GE
+pacman -S --noconfirm steam
+# References:
+# None yet
 
 # Steam controllers udev rules
 curl -sSL https://raw.githubusercontent.com/ValveSoftware/steam-devices/master/60-steam-input.rules -o /etc/udev/rules.d/60-steam-input.rules
 udevadm control --reload-rules
+
+################################################
+##### Other game launchers
+################################################
 
 # Heroic Games Launcher
 flatpak install -y flathub com.heroicgameslauncher.hgl
 
 # Lutris
 flatpak install -y flathub net.lutris.Lutris
+
+################################################
+##### Roblox launcher
+################################################
+
+git clone --depth=1 https://aur.archlinux.org/grapejuice-git.git /grapejuice-git
+chown -R ${NEW_USER}:${NEW_USER} grapejuice-git
+cd grapejuice-git
+sudo -u ${NEW_USER} makepkg -si --noconfirm
+cd ..
+rm -rf grapejuice-git
