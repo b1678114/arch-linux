f28b5832eae75f31f45fffdb1744ce745ce3717e
youdontknowdemo <36462734+youdontknowdemo@users.noreply.github.com>
2023-04-01 17:31:30 -0600

    Update overlay.sh

--- overlay.sh
+++ overlay.sh
@@ -1,8 +1,26 @@
 #!/usr/bin/bash
 
-####################################
-#### install mangohud and goverlay
-####################################
+################################################
+##### Enable Chaotic AUR repository
+################################################
+# References: 
+# vulkan dependies seem broken for my build at install, suddenly, it worked many times before, Could't debug fully, CAUR Works..
+
+# Get CAUR Key
+pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key FBA220DFC880C036
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
+# Enable CAUR by addition
+echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
+
+# update packagelists
+sudo pacman -Syy
+sudo paru -Syy
+
+################################################
+##### Get mangohud and goverlay
+################################################
 
 sudo -u view paru -S --noconfirm mangohud
 sudo -u view paru -S --noconfirm goverlay
