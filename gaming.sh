ee39181ffe6d8faef9f02ba5f187943f84d28f5e
youdontknowdemo <36462734+youdontknowdemo@users.noreply.github.com>
2023-03-31 22:41:15 -0600

    Update gaming.sh

--- gaming.sh
+++ gaming.sh
@@ -1,79 +1,41 @@
 #!/usr/bin/bash
 
 ################################################
-##### Variable Check
+##### MangoHud
 ################################################
 
-read -p "Username: " NEW_USER
-export NEW_USER
-
-################################################
-##### Enable multilib repository
-################################################
 # References:
-# None yet
+# https://github.com/flathub/com.valvesoftware.Steam.Utility.MangoHud
 
-# enable multilib by addition
-echo -e "\n[multilib]\nInclude = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
+# Install MangoHud
+flatpak install -y flathub org.freedesktop.Platform.VulkanLayer.MangoHud/x86_64/22.08
 
-# update packagelists
-sudo pacman -Syy
+# Configure MangoHud
+mkdir -p /home/${NEW_USER}/.config/MangoHud
 
-################################################
-##### Get headers for Nvidia to use DKMS
-################################################
+tee /home/${NEW_USER}/.config/MangoHud/MangoHud.conf << EOF
+engine_version
+vulkan_driver
+EOF
 
-sudo pacman -S --noconfirm linux-headers linux-lts-headers
-
-################################################
-##### Nvidia Installer
-################################################
-# References:
-# None yet
-
-git clone https://github.com/Frogging-Family/nvidia-all
-chown -R ${NEW_USER}:${NEW_USER} nvidia-all
-cd nvidia-all
-sudo -u ${NEW_USER} makepkg -si
-cd ..
-rm -rf nvidia-all
+# Allow Flatpaks to access MangoHud configs
+flatpak override --filesystem=xdg-config/MangoHud:ro
 
 ################################################
-##### Steam
+##### Platforms
 ################################################
-# References:
-# None yet
 
-pacman -S --noconfirm steam
+# Steam
+flatpak install -y flathub com.valvesoftware.Steam
+flatpak install -y flathub com.valvesoftware.Steam.Utility.gamescope
+flatpak install -y flathub com.valvesoftware.Steam.CompatibilityTool.Proton-GE
 
 # Steam controllers udev rules
 curl -sSL https://raw.githubusercontent.com/ValveSoftware/steam-devices/master/60-steam-input.rules -o /etc/udev/rules.d/60-steam-input.rules
 udevadm control --reload-rules
-
-################################################
-##### Other game launchers
-################################################
 
 # Heroic Games Launcher
 flatpak install -y flathub com.heroicgameslauncher.hgl
 
 # Lutris
 flatpak install -y flathub net.lutris.Lutris
-
-################################################
-##### Roblox launcher
-################################################
-
-git clone --depth=1 https://aur.archlinux.org/grapejuice-git.git ./grapejuice-git
-chown -R ${NEW_USER}:${NEW_USER} ./grapejuice-git
-cd ./grapejuice-git
-sudo -u ${NEW_USER} makepkg -si --noconfirm
-cd ..
-rm -rf ./grapejuice-git
-
-################################################
-##### prime-run
-################################################
-
-# Install prime-run command via nvidia-prime
-pacman -S --noconfirm nvidia-prime
