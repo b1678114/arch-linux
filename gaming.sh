f4381f1f5ca60511e25a1e28fa47cf72355f2922
youdontknowdemo <36462734+youdontknowdemo@users.noreply.github.com>
2023-04-01 17:30:34 -0600

    Update gaming.sh

--- gaming.sh
+++ gaming.sh
@@ -76,24 +76,6 @@ rm -rf ./grapejuice-git
 pacman -S --noconfirm nvidia-prime
 
 ################################################
-##### Enable Chaotic AUR repository
-################################################
-# References: 
-# vulkan dependies seem broken for my build at install, suddenly, it worked many times before, Could't debug fully, CAUR Works..
-
-# Get CAUR Key
-pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
-pacman-key --lsign-key FBA220DFC880C036
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-
-# Enable CAUR by addition
-echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
-
-# update packagelists
-sudo pacman -Syy
-sudo paru -Syy
-
-################################################
 ##### Get mangohud and goverlay
 ################################################
 
