2b850cf97e8404bfcde04e84e9b66a658201545d
youdontknowdemo <36462734+youdontknowdemo@users.noreply.github.com>
2023-04-01 17:28:31 -0600

    Create overlay.sh

---
+++ overlay.sh
@@ -0,0 +1,10 @@
+install mangohud and goverlay
+sudo -u view paru -S --noconfirm mangohud
+sudo -u view paru -S --noconfirm goverlay
+
+# undo the CAUR again for now
+sudo sed -i '/\[chaotic-aur\]/,+1d' /etc/pacman.conf
+
+# update packagelists
+sudo pacman -Syy
+sudo paru -Syy
