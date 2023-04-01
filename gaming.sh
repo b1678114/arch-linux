a4aeb853496efe13ff586df95fc2ccfe8ce37f20
youdontknowdemo <36462734+youdontknowdemo@users.noreply.github.com>
2023-04-01 17:28:04 -0600

    Update gaming.sh

--- gaming.sh
+++ gaming.sh
@@ -97,13 +97,5 @@ sudo paru -Syy
 ##### Get mangohud and goverlay
 ################################################
 
-# install mangohud and goverlay
-sudo -u view paru -S --noconfirm mangohud
-sudo -u view paru -S --noconfirm goverlay
-
-# undo the CAUR again for now
-sudo sed -i '/\[chaotic-aur\]/,+1d' /etc/pacman.conf
-
-# update packagelists
-sudo pacman -Syy
-sudo paru -Syy
+# Issue #1 the overlay script only seems to work after system is rebooted into GUI or something, 
+ehco "please reboot, log in and run cd && ./overlay.sh
