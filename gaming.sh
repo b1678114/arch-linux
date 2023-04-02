39145e8f8b3c72ad1bd1c27c28c62369ab07949f
youdontknowdemo <36462734+youdontknowdemo@users.noreply.github.com>
2023-04-02 09:48:48 -0600

    Update gaming.sh

--- gaming.sh
+++ gaming.sh
@@ -84,7 +84,7 @@ pacman -S --noconfirm nvidia-prime
 # Get CAUR Key
 pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key FBA220DFC880C036
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
 # Enable CAUR by addition
 echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
