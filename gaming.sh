f44a0aa4e37b16d6132a6eb9e3e949de3908353c
youdontknowdemo <36462734+youdontknowdemo@users.noreply.github.com>
2023-04-01 18:59:23 -0600

    Update gaming.sh

--- gaming.sh
+++ gaming.sh
@@ -76,11 +76,36 @@ rm -rf ./grapejuice-git
 pacman -S --noconfirm nvidia-prime
 
 ################################################
+##### Enable Chaotic AUR repository
+################################################
+# References: 
+# vulkan dependies seem broken for my build at install, suddenly, it worked many times before, Could't debug fully, CAUR Works..
+
+# Get CAUR Key
+pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key FBA220DFC880C036
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
+# Enable CAUR by addition
+echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
+
+# update packagelists
+sudo pacman -Syy
+sudo paru -Syy
+
+################################################
 ##### Get mangohud and goverlay
 ################################################
 
-# Issue #1 the overlay script only seems to work after system is rebooted into GUI or something, 
-ehco "please reboot, log in and run cd && ./overlay.sh
-curl  --tlsv1.2 -fsSL https://raw.githubusercontent.com/youdontknowdemo/arch-linux/devel/overlay.sh -O
-cp ./overlay.sh /home/{NEW_USER}/overlay.sh
-chmod 0755 /home/{NEW_USER}/overlay.sh
+sudo -u view paru -S --noconfirm mangohud
+sudo -u view paru -S --noconfirm goverlay
+
+# undo the CAUR again for now
+sudo sed -i '/\[chaotic-aur\]/,+1d' /etc/pacman.conf
+
+# update packagelists
+sudo pacman -Syy
+sudo paru -Syy
+
+# reglu something that came unglued
+sudo pacman -S --noconfirm glu
