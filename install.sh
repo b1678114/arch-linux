44f3cd1bf6e7cb323537b71d32094d715b5d8a1f
youdontknowdemo <36462734+youdontknowdemo@users.noreply.github.com>
2023-04-25 11:42:34 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -126,16 +126,16 @@ genfstab -U /mnt >> /mnt/etc/fstab
 
 # Configure system
 mkdir -p /mnt/install-arch/extra
-curl --tlsv1.2 -fsSL https://raw.githubusercontent.com/youdontknowdemo/arch-linux/devel-hp-ryzen/extra/firefox.js -O
+curl --tlsv1.2 -fsSL https://raw.githubusercontent.com/youdontknowdemo/arch-linux/headless/extra/firefox.js -O
 cp ./firefox.js /mnt/install-arch/firefox.js
 chmod 0755 /mnt/install-arch/firefox.js
-curl --tlsv1.2 -fsSL https://raw.githubusercontent.com/youdontknowdemo/arch-linux/devel-hp-ryzen/gnome.sh -O
+curl --tlsv1.2 -fsSL https://raw.githubusercontent.com/youdontknowdemo/arch-linux/headless/gnome.sh -O
 cp ./gnome.sh /mnt/install-arch/gnome.sh
 chmod 0755 /mnt/install-arch/gnome.sh
-curl --tlsv1.2 -fsSL https://raw.githubusercontent.com/youdontknowdemo/arch-linux/devel-hp-ryzen/gaming.sh -O
+curl --tlsv1.2 -fsSL https://raw.githubusercontent.com/youdontknowdemo/arch-linux/headless/gaming.sh -O
 cp ./gaming.sh /mnt/install-arch/gaming.sh
 chmod 0755 /mnt/install-arch/gaming.sh
-curl  --tlsv1.2 -fsSL https://raw.githubusercontent.com/youdontknowdemo/arch-linux/devel-hp-ryzen/setup.sh -O
+curl  --tlsv1.2 -fsSL https://raw.githubusercontent.com/youdontknowdemo/arch-linux/headless/setup.sh -O
 cp ./setup.sh /mnt/install-arch/setup.sh
 chmod 0755 /mnt/install-arch/setup.sh
 arch-chroot /mnt/ /bin/bash /install-arch/setup.sh
